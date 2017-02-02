3234d77ffe3120c872dbd0a728e019ff3910cd63
Haweya <haweyoj@student.matnat.uio.no>
2017-02-02 15:25:52 +0100

    .

--- GOL - Game of Life/src/MVC/Controller.java
+++ GOL - Game of Life/src/MVC/Controller.java
@@ -13,7 +13,7 @@ public class Controller implements Initializable{
     @Override
     public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
 
-    	//hei kjkk
+    	//hei nfkdlsl
     }
 
     public void StartNewGame() {
