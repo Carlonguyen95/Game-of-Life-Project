6ca1f528a79829c3ac64bd549164f7e4ef543fc7
Carlo Nguyen <s315613@stud.hioa.no>
2017-02-10 12:24:45 +0100

    Byttet navn fra Main til Launcher

--- GOL - Game of Life/src/MVC/Launcher.java
+++ GOL - Game of Life/src/MVC/Launcher.java
@@ -3,22 +3,20 @@ package MVC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 import javafx.stage.*;
 
-public class Main extends Application {
+public class Launcher extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+    	
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        root.setVisible(true);
 
         Scene scene = new Scene(root, 800, 600);
         
         primaryStage.setTitle("Game of Life");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
