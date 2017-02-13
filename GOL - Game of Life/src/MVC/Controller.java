70adcb5314b2df6c6cdb6b99ab53e781c50c36f7
idriso <mokk015@hotmail.com>
2017-02-13 16:32:23 +0100

    koora

--- GOL - Game of Life/src/MVC/Controller.java
+++ GOL - Game of Life/src/MVC/Controller.java
@@ -5,65 +5,100 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Controller implements Initializable{
-	
-	//Objekter relatert til GUI
-	@FXML private Canvas graphics;
-	@FXML private Slider sizeSlider;
-	private List<Point> plist;
 
-	//Denne metoden starter ved oppstart
     @Override
-    public void initialize(java.net.URL location, java.util.ResourceBundle resources) {
-        sizeSlider.setValue(5.0);
-    	plist = new ArrayList<Point>();
-    	draw();
+    public void initialize(java.net.URL location,java.util.ResourceBundle resources){
+
+        gc = graphics.getGraphicsContext2D();
+        colorChanger.setValue(Color.BLACK);
+
+        start_Game();
+
+
+
     }
-    
-    //Henter x og y kords. Bestemmelse på farge og strl av obj.
-    private static class Point {
-    	public double x, y;
-    	public void draw(GraphicsContext gc, Paint color, double size) {
-			gc.setFill(Color.BLACK);
-    		gc.fillRect(x-15, y-15, 30, 30);
-    	}
+
+    @FXML private Button startButton;
+    @FXML private Canvas graphics;
+    @FXML private ColorPicker colorChanger;
+    @FXML private Slider sizeSlider;
+
+    GraphicsContext gc;
+    private int cellSize = 10;
+
+
+    private void start_Game() {
+
+        drawGrid();
     }
-    
-    public void draw() {
-    	GraphicsContext gc = graphics.getGraphicsContext2D();
-        for ( Point p : plist ) {
-            p.draw(gc, Color.BLACK, sizeSlider.getValue());
-        }
-    }
-    
-    public void mouseClicked(MouseEvent event) {
-        Point p = new Point();
-        p.x = event.getX();
-        p.y = event.getY();
-        plist.add(p);
-    	draw();
-    }
-    
-    public void exitEvent(ActionEvent event) {
+
+    public void closeProgram(ActionEvent event) {
         System.exit(0);
     }
-    
-    public void clearEvent(ActionEvent event) {
-        plist.clear();
+
+    private byte[][] board = {
+            {1, 0, 0, 1},
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {1, 0, 0, 1}
+    };
+
+    public void drawGrid() {
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(1);
+
+        for (double x = 0; x < 600; x += cellSize) {
+            gc.strokeLine(x, 1000, x, 0);
+        }
+        for (double y = 0; y < 600; y += cellSize) {
+            gc.strokeLine(0, y, 1000, y);
+        }
+    }
+
+    public void drawBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+                if (board[i][j] == 1) {
+                    gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize);
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public void draw() {
+        drawGrid();
+        drawBoard();
+    }
+
+    @FXML
+    public void colorChange() {
+
+        gc.setFill(colorChanger.getValue());
+        drawBoard();
+    }
+
+    @FXML
+    public void sizeChange() {
+        cellSize = (int) sizeSlider.getValue();
+        gc.clearRect(0,0,600,315);
         draw();
     }
 
-    public void newSizeEvent(MouseEvent e) {
-        draw();
+    @FXML
+    public void clearBoard() {
+        gc.clearRect(0,0,600,315);
+        sizeSlider.setValue(10);
+        cellSize = (int) sizeSlider.getValue();
+        colorChanger.setValue(Color.BLACK);
+        gc.setFill(colorChanger.getValue());
+        drawGrid();
     }
-    
-
 }
-
