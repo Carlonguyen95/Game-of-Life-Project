098aac3d68e599d008962d94e1ff4eb341f9d064
Carlo Nguyen <s315613@stud.hioa.no>
2017-03-07 16:06:24 +0100

    07.03.17 Update

--- GOL - Game of Life/src/golController/BoardCtrl.java
+++ GOL - Game of Life/src/golController/BoardCtrl.java
@@ -35,10 +35,10 @@ public class BoardCtrl implements Initializable{
 	private Timeline timeline = new Timeline();
     private byte[][] board = new byte[100][100];
     /*private byte[][] board = {
-    		{ 1, 0, 0, 1 },
+    		{ 0, 0, 0, 0 },
     		{ 0, 1, 1, 0 },
     		{ 0, 1, 1, 0 },
-    		{ 1, 0, 0, 1 }
+    		{ 0, 0, 0, 0 }
     		 };*/
 	
     @Override
@@ -193,28 +193,6 @@ public class BoardCtrl implements Initializable{
     	timeline.pause();
     }
     
-    
-    public void nextGeneration() {
-    	
-    	for (int i = 0; i < board.length; i++) {
-    		
-            for (int j = 0; j < board[i].length; j++) {
-            	                
-                if (board[i][j] == 1) {
-                	
-                	board[i][j] = 0;
-                    gc.clearRect(i*cellSize, j*cellSize,cellSize,cellSize);
-
-                }else {
-                	board[i][j] = 1;
-                    gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize);
-
-                }
-            }
-        }
-    	draw();
-    }
-    
     public void Animation() {
     	
     	timeline.setCycleCount(Animation.INDEFINITE);
@@ -253,6 +231,94 @@ public class BoardCtrl implements Initializable{
         	timeline.setRate(4);
     	}
     }
+    
+    public void nextGeneration() {
+    	
+		for (int i = 0; i < board.length; i++) {
+			
+	        for (int j = 0; j < board[i].length; j++) {
+	        	
+	              if(board[i][j] == 0) { //the cell is dead
+	                  if(neighbours(i,j) == 3) { 
+	                    gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize); 
+	                    board[i][j] = 1; 
+	                  }
+	                  else board[i][j] = 0;
+	              }
+	              
+	              else { // the cell is alive 
+	                  if(neighbours(i,j)< 2 || neighbours(i,j) > 3) { 
+	                    gc.clearRect(i*cellSize, j*cellSize,cellSize,cellSize); 
+	                    board[i][j] = 0; 
+	                  }                
+	                  else board[i][j] = 1;
+	            } 
+	        	
+	        	/*// Any dead cell with exactly three live neighbors will come to life
+	        	if(board[i][j] == 0) {
+	        		if(neighbours(i,j) == 3) {
+	        			gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize);
+	        			board[i][j] = 1;
+	        		}           	
+	            }
+	        	
+	        	// Any live cell with two or three live neighbors lives, unchanged, to the next generation
+	        	if(board[i][j] == 1) {
+	        		if(neighbours(i,j) == 2 || neighbours(i,j) == 3) {
+	        			gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize);
+	        			board[i][j] = 1;
+	        		}
+	        	}
+	        	
+	        	// Any live cell with more than three live neighbors dies
+	        	if(board[i][j] == 1) {
+	        		if(neighbours(i,j) > 3) {
+	        			gc.clearRect(i*cellSize, j*cellSize,cellSize,cellSize);
+	        			board[i][j] = 0;
+	        		}           	
+	            }
+	        	
+	        	// Any live cell with fewer than two live neighbors dies
+	        	if(board[i][j] == 1) {
+	        		if(neighbours(i,j) < 2) {
+	        			gc.clearRect(i*cellSize, j*cellSize,cellSize,cellSize);
+	        			board[i][j] = 0;
+	              	}                
+	        	}
+	        	}*/
+	        	
+	        }	
+		}     
+				draw();
+}
+
+	public int neighbours(int x, int y) {
+	
+		int nr = 0; //so that the cell doesn't count itself
+		
+		for(int i = x-1; i <= x+1; i++){
+			
+	        if(i > 0 && i < board.length){ //cells on the edges (rows)
+	        	
+	            for(int j = y-1; j <= y + 1; j++){
+	            	
+	                if(j > 0 && j < board[i].length){ // cells on the edges (columns)
+	                	
+	                    if (board[i][j] == 1) {
+	                    	
+	                        nr++;
+	                    }
+	                }
+	            }
+	        }
+	    }
+		
+		return nr;
+	
+}
+
+	
+
 
 }
     	
