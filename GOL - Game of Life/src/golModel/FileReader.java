0c1bb96accba788ec201d106922620df2ab62a64
Haweya <haweyoj@student.matnat.uio.no>
2017-03-17 14:10:44 +0100

    lagt til readFromDisk metode

--- GOL - Game of Life/src/golModel/FileReader.java
+++ GOL - Game of Life/src/golModel/FileReader.java
@@ -1,7 +1,20 @@
 package golModel;
 
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+
+import java.nio.*;
 import java.io.*;
 import java.util.*;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
 public class FileReader {
 	
 	private int rows;
@@ -13,21 +26,52 @@ public class FileReader {
 		
 	}
 	
-	public byte[][] readFromDisk(String filename) throws IOException {
+	public byte[][] readFromDisk() throws Exception {
+		
+		Path diskFile = Paths.get(chooseTextFile());
 
 		byte[][] board = new byte[rows][columns]; 
+		int counter = 0;
+		
 		try {
 			
+			InputStream lineCounter = Files.newInputStream(diskFile);
+			int line = lineCounter.read();
+			while (line != -1) { // number of lines in the file
+				counter ++;
+			}
+			
+			byte[] b = new byte[counter];
+			
+			InputStream fileReader = Files.newInputStream(diskFile);
+			int lines = fileReader.read();
+			while (lines != -1) { // reads file content
+				b = Files.readAllBytes(diskFile);
+
+			}
 			
 			
+			for(int i = 0; i>board.length; i+=2) {
+				for(int j = 1; j> board[i].length; j++) {
+					board[b[i]][b[j]] = 1;
+				}
+			}
+			
+			
+			return board;
 		}
 		
-		catch (FileNotFoundException f) { 
-			
+		catch (Exception e) { // filenotfound exception (fix) 
+
 			//handle exception
-			return board;
+			
 			}
 		return board;
+	}
+	
+	public String chooseTextFile() {
+		//add JFileChooser
+		return null;
 	}
 	
 }
