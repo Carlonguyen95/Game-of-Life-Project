088cd81da64eee9a393f893efd99312dd915deec
Carlo <Carlo@10.253.10.182>
2017-03-31 15:20:43 +0200

    Javadocumented

--- GOL - Game of Life/src/golController/Control.java
+++ GOL - Game of Life/src/golController/Control.java
@@ -23,13 +23,17 @@ import javafx.util.Duration;
 import javafx.scene.control.ToggleButton;
 
 import java.io.File;
-import java.net.URL;
-import java.net.URLConnection;
-
 import javax.swing.JOptionPane;
 
 import golClasses.FileReader;
 
+/**
+ *
+ * 
+ * @author Carlo Nguyen
+ * @author Haweya Jama
+ * @author Idris Milan
+ */
 public class Control implements Initializable{
 
     @FXML private Canvas graphics;
@@ -53,71 +57,64 @@ public class Control implements Initializable{
     public void initialize(java.net.URL location,java.util.ResourceBundle resources){
 
         gc = graphics.getGraphicsContext2D();
-        
-        colorChangerBtn.setValue(Color.GREEN);
+        colorChangerBtn.setValue(Color.BLACK);
         sizeSliderBtn.setValue(10.0);
         speedBtn.setValue("Speed"); 
         speedBtn.setItems(speedList);
 
-        start_Game();
+        draw();
     }
     
     public void closeProgram(ActionEvent event) {
         System.exit(0);
     }
     
-    public void start_Game() {  	
-    	draw();
-    }
-    
 	/**
-	 *	
-	 *
-	 *
-	 * Carlo
-	 *
-	 *
-	 * 
+	 * This method is connected to a togglebutton.
+	 * Allows the user to start the game, and pause it anytime.
+	 * When the Start-button is pressed, Animation() method will execute.
 	 */
-    
     public void toggleBtn() {
     	
     	if(startPauseBtn.isSelected()) {
     		startPauseBtn.setText("Pause");
     		
-    		run();
+    		Animation();
     	}else {
     		startPauseBtn.setText("Start");
-    		pause();
+    		timeline.pause();
     	}
     }
 
-    @FXML
+    /**
+     * This method is connected to a colorpicker.
+     * Allows the user to select color to the board and drawn cells.
+     * The color will be changed after each sequence of drawBoard() is executed.
+     * gc is a variable assigned to Canvas, and returns the GraphicsContext associated with this Canvas.
+     */
     public void colorChange() {
 
         gc.setFill(colorChangerBtn.getValue());
         drawBoard();
     }
 
-    @FXML
+    /**
+     * This method is connected to a slider.
+     * Allows the user to select size to the board and drawn cells.
+     * The method sets the new size to cellSize, gc clears the board, then draw() is executed.
+     * And draws everything to the screen.
+     */
     public void sizeChange() {
     	
         cellSize = (int) sizeSliderBtn.getValue();
     	gc.clearRect(0, 0, graphics.getWidth(), graphics.getHeight());
-        
-    	for (int i = 0; i < board.length; i++) {
-    		
-            for (int j = 0; j < board[i].length; j++) {
-            	                
-                if (board[i][j] == 1) {
-                	
-                }
-            }
-        }
-    	
-    	draw();
+        draw();
     }
     
+    /**
+     * This method is connected to a combobox.
+     * Allows user to select the direction/speed at which the Animation is expected to be played.
+     */
     public void speedSlction() { 
         
         if(speedBtn.getValue().equals("1x")) { 
@@ -131,76 +128,92 @@ public class Control implements Initializable{
         } 
       }    
     
-    @FXML
+    /**
+     * This method is connected to a button.
+     * Allows the user to upload a pattern-file into the board.
+     * Format are filtered to .lif and .life
+     * When pattern is loaded, clearBoard() clears the current board. 
+     * Board gets assigned to new values and being drawn to the screen.
+     * 
+     * @throws Exception when no file is selected.
+     */
     public void uploadPattern() throws Exception { //uploads pattern from file
     	
-    	FileChooser file = new FileChooser();
-    	file.getExtensionFilters().addAll(
-    			new ExtensionFilter("LIF, LIFE", "*.lif", "*.life"));
-    	
-    	File selectedFile = file.showOpenDialog(null);
-    	listview.getItems().add(selectedFile.getName());
-    	
-    	if(selectedFile != null) {
-    		FileReader f = new FileReader(board);
-        	String path = selectedFile.getAbsolutePath();
-        	
-        	clearBoard();
-        	board = f.readFromDisk(path);
-        	drawBoard();
-        	
-    	}
+	    	FileChooser file = new FileChooser();
+	    	// Filtered to only show files with format .lif and .life
+	    	file.getExtensionFilters().addAll(
+	    			new ExtensionFilter("LIF, LIFE", "*.lif", "*.life"));
+	    	
+	    	// Shows the name of the uploaded file
+	    	File selectedFile = file.showOpenDialog(null);
+	    	
+	    	if(selectedFile != null) {
+	    		listview.getItems().add(selectedFile.getName());
+	    		FileReader f = new FileReader(board);
+	        	String path = selectedFile.getAbsolutePath();
+	        	
+	        	clearBoard();
+	        	board = f.readFromDisk(path);
+	        	drawBoard();
+	    	}
     }
     
-    @FXML
+    /**
+     * This method is connected to a button.
+     * Allows user to input a pattern via URL into the board.
+     * When URL is loaded, clearBoard() clears the current board. 
+     * Board gets assigned to new values and being drawn to the screen.
+     * 
+     * @throws Exception when no URL is inserted.
+     */
     public void loadURL() throws Exception {
     	String url = new String();
     	url = JOptionPane.showInputDialog(null, "Please enter a URL");
     	
-    	clearBoard();
-    	FileReader u = new FileReader(board);
-    	board = u.readFromURL(url);
-    	drawBoard();
+    	if(url != null) {
+    		clearBoard();
+    		FileReader u = new FileReader(board);
+    		board = u.readFromURL(url);
+    		drawBoard();
+    	}
     }
-   
     
     /**
-     *	Methods for USERs 
-     *
+     * This method allows the user to selfdraw cells on the board, by dragclick with the mouse.
+     * Each dragclick on the board will get the pointers coordinate X and Y relative to the Board.
+     * The value X and Y get casted into integer values, then put into Board's array.
+     * Board[x][y] is equal to 1 means there is a living cell on that coordinate.
      */
-
     public void drawCell(MouseEvent event) {
     	
     	double x = event.getX()/cellSize;
     	double y = event.getY()/cellSize;
   
     	board[(int)x][(int)y] = 1;
-    	drawBoard(); 	
-    }
-
-    public void run() {    	
-    	Animation();
-    }
-    
-    public void pause() {
-    	timeline.pause();
+    	drawBoard();
     }
     
     /**
-     *	Draw and clear methods for board, grid and cells
-     *
+     * This is a "helping-method" which contains two main draw-methods.
+     * drawGrid draws the grid of the board.
+     * drawBoard draws the board's array.
      */
-
     public void draw() {
     	
         drawGrid();
         drawBoard();
     }
     
+    /**
+     * This method draws the grid of the board.
+     * for-loops for X and Y, which adds the size of the cell each iterate to the grid
+     * according to the height and width of the Canvas.
+     */
     public void drawGrid() {
 
     	gc.setFill(colorChangerBtn.getValue());
         gc.setStroke(colorChangerBtn.getValue());
+        // Thickness of the grid
         gc.setLineWidth(1);
 
         for (int x = 0; x < graphics.getWidth(); x += cellSize) {
@@ -211,14 +224,18 @@ public class Control implements Initializable{
         }
     }
 
+    /**
+     * This method draws the board
+     * If the X and Y is equal to 1 there is a living cell.
+     * 
+     * @param i is point to X axis
+     * @param j is point to Y axis
+     */
     public void drawBoard() {
     	
     	for (int i = 0; i < board.length; i++) {
-    		
-            for (int j = 0; j < board[i].length; j++) {
-            	                
+            for (int j = 0; j < board[i].length; j++) {                
                 if (board[i][j] == 1) {
-                	
                     gc.fillRect(i*cellSize, j*cellSize,cellSize,cellSize);
                     gc.setFill(colorChangerBtn.getValue());
                 }
@@ -226,17 +243,20 @@ public class Control implements Initializable{
         }
     }
 
-    @FXML
+    /**
+     * This method clears the current Board
+     * Clears the board first.
+     * Then a for-loop that search board for any living cells
+     * if true, then set that coordinate [x][y] equal to 0
+     * Draw grid again and stop the Animation.
+     */
     public void clearBoard() {
     	
     	gc.clearRect(0, 0, graphics.getWidth(), graphics.getHeight());
     	
     	for (int i = 0; i < board.length; i++) {
-    		
-            for (int j = 0; j < board[i].length; j++) {
-            	                
+            for (int j = 0; j < board[i].length; j++) {                
                 if (board[i][j] == 1) {
-                	
                     gc.clearRect(i*cellSize, j*cellSize,cellSize,cellSize);
                     board[i][j] = 0;
                 }
@@ -253,10 +273,9 @@ public class Control implements Initializable{
     }
     
     /**
-     *	Next Generation methods
+     * 
      * 
      */
-    
     public void nextGeneration() { 	
     	byte[][] updated = new byte[board.length][board[0].length];
     	
